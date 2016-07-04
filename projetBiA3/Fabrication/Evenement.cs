48d59d89996ddd675ed74f24b9e6c6b40db2c57f
Antoine Delia <antoine.delia@gmail.com>
2016-07-04 16:44:47 +0200

    Add SQL scripts

---
+++ projetBiA3/Fabrication/Evenement.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fabrication
+{
+    public class Evenement
+    {
+        private Machine machine;
+        private DateTime dateDebutEvenement;
+        private DateTime dateFinEvenement;
+        private Event typeEvenement;
+        private String commentaires;
+
+        public enum Event {Panne, Maintenance};
+
+        public Machine Machine { get; set; }
+        public DateTime DateDebutEvenement { get; set; }
+        public DateTime DateFinEvenement { get; set; }
+        public Event TypeEvenement { get; set; }
+        public String Commentaires { get; set; }
+
+    }
+}
---
+++ projetBiA3/Fabrication/Machine.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fabrication
+{
+    public class Machine
+    {
+        private Int32 rendementPrevu;
+        private Int32 rendementReel;
+
+        public Int32 RendementPrevu { get; set; }
+        public Int32 RendementReel { get; set; }
+    }
+}
